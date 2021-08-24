d8d9199abe3b6a2246aa27c4f85624aaa6710cf5
Dan Lee <dandhlee@google.com>
2021-08-24 22:28:38 +0000

    chore: update owlbot

--- owlbot.py
+++ owlbot.py
@@ -90,7 +90,8 @@ s.replace(
 )
 
 s.replace(
-  "samples/*",
-  "master",
-  "main"
+  "README.rst",
+  "google-cloud-python/blob/main/README.rst",
+  "google-cloud-python/blob/master/README.rst"
 )
+
